13215aae2fa51b0253125d3ea9b0714f673c48ed
Ludvig Sundström <lud.sund@gmail.com>
2018-10-01 16:48:11 +0200

    Get list of process instances

--- src/camunda_cli_tool/process_definition.clj
+++ src/camunda_cli_tool/process_definition.clj
@@ -1,4 +1,6 @@
-(ns camunda-cli-tool.process-definition)
+(ns camunda-cli-tool.process-definition
+  (:require [clojure.data.json :as j]
+            [camunda-cli-tool.http :as http]))
 
 (defn show-unique-process-definitions []
   :todo)
--- src/camunda_cli_tool/process_instance.clj
+++ src/camunda_cli_tool/process_instance.clj
@@ -1,4 +1,26 @@
-(ns camunda-cli-tool.process-instance)
+(ns camunda-cli-tool.process-instance
+  (:require [clojure.data.json :as j]
+            [camunda-cli-tool.http :as http]))
+
+(def rest-endpoint "process-instance")
+
+(defrecord ProcessInstance [id definition-id])
+
+(defn show [pinst]
+  (str (:id pinst)))
+
+(defn keywordize [m]
+  (into {}
+        (map vector
+                (map keyword (keys m))
+                (vals m))))
+
+(defn json->ProcessInstance [j]
+  (select-keys (keywordize j) [:id]))
+
+(defn list []
+  (map json->ProcessInstance (j/read-str (:body (http/make-rest-call rest-endpoint)))))
 
 (defn show-process-instances []
-  :todo)
+  (doseq [x (list)]
+    (println (:id x))))
