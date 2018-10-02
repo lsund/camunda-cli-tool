375ada1ea9710a1d88cdc9cc43277afd897ee415
Ludvig Sundström <lud.sund@gmail.com>
2018-10-02 16:54:48 +0200

    Navigate back

--- src/camunda_cli_tool/core.clj
+++ src/camunda_cli_tool/core.clj
@@ -6,8 +6,7 @@
 (def default-keymap
   {:title "Main Menu"
    \d {:description "List Process Definitions" :next (pdef/most-recent-keymap)}
-   \i {:description "List Active Process Instances" :next (pinst/keymap)}
-   \q {:description "Quit" :next nil}})
+   \i {:description "List Active Process Instances" :next (pinst/keymap)}})
 
 (defn show-title [title]
   (let [padding (apply str (take (quot (- 80 (count title)) 2) (repeat \.)))]
@@ -20,28 +19,34 @@
       (= :title x) nil
       (char? x) (printf "(%c) %s\n" x (:description o))
       (number? x) (printf "(%d) %s\n" x (:description o))
-      :default (throw (Exception. (str "Unknown type: " (type x)))))))
+      :default (throw (Exception. (str "Should not happen. Type: " (type x))))))
+  (println "(b) Back")
+  (println "(q) Quit"))
 
 (defn repl
-  [keymap]
-  (println (apply str (repeat 80 "-")))
-  (print-keymap keymap)
-  (flush)
-  (let [l (read-line)
-        c (first l)]
-    (if (= c \q)
-      (println "Bye")
-      (do
-        (if-let [new-keymap (get-in keymap [c :next])]
-          (repl new-keymap)
-          (let [fun (get-in keymap [c :function])
-                args (get-in keymap [c :args])]
-            (println "Result: " (apply fun args))
-            (repl keymap)))))))
+  [keymaps]
+  (let [keymap (first keymaps)]
+    (println (apply str (repeat 80 "-")))
+    (print-keymap keymap)
+    (flush)
+    (let [l (read-line)
+          c (first l)]
+      (case c
+        \q (println "Bye")
+        \b (if-let [previous (next keymaps)]
+             (repl previous)
+             (repl keymaps))
+        (do
+          (if-let [new-keymap (get-in keymap [c :next])]
+            (repl (conj keymaps new-keymap))
+            (let [fun (get-in keymap [c :function])
+                  args (get-in keymap [c :args])]
+              (println "Result: " (apply fun args))
+              (repl keymaps))))))))
 
 (defn run []
-  (println "Use 'q' for quit")
-  (repl default-keymap))
+  (println "Use the keys in (brackets) to navigate.")
+  (repl (list default-keymap)))
 
 (defn -main [& args]
   (run))
