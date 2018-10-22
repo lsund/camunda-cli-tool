33c49ae7b0581361743564c0bc4187d7e18ed0eb
Ludvig Sundström <lud.sund@gmail.com>
2018-10-22 14:12:22 +0200

    Update no variable exception message

--- src/camunda_cli_tool/config.clj
+++ src/camunda_cli_tool/config.clj
@@ -1,14 +1,19 @@
 (ns camunda-cli-tool.config
   (:require [clojure.edn :as edn]))
 
+(def config-file "resources/edn/config.edn")
+
 (defn load []
-  (-> "resources/edn/config.edn" slurp edn/read-string))
+  (-> config-file slurp edn/read-string))
 
 (defn process-variables [key]
   (if-let [variables (get-in (edn/read-string (slurp "resources/edn/process-variables.edn"))
                              [key :default])]
     variables
-    (throw (Exception. "No configured variables for process"))))
+    (throw (Exception. (str "No configured variables for process.\n"
+                            "Default variables need to be cofigured in file: \n"
+                            config-file "\n"
+                            "See https://github.com/lsund/camunda-cli-tool for more information.")))))
 
 (defn required-keys [key]
   (if-let [keys (get-in (edn/read-string (slurp "resources/edn/process-variables.edn"))
